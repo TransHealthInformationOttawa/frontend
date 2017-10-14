8afe983620db26477e3519a72522a14e4351354b
Maëlys McArdle <airseal@hotmail.com>
2017-10-14 11:35:19 -0400

    Added JS file for API queries.

---
+++ src/apiqueries.js
@@ -0,0 +1,31 @@
+function getPersons() {
+
+}
+
+function addPerson(name, phoneNumber, schedule, messages) {
+
+}
+
+function updatePerson(personId, phoneNumber, schedule, messages) {
+
+}
+
+function deletePerson(personId) {
+
+}
+
+function getMessages(personId) {
+
+}
+
+function addMessage(personId, message) {
+
+}
+
+function deleteMessage(personId, messageId) {
+
+}
+
+function updateMessage(personId, messageId, message) {
+
+}
