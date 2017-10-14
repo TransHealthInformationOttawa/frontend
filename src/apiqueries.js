fbe3e5ca9bcfcc5b0c4c86cd145051ac8fc7cd2a
Maëlys McArdle <airseal@hotmail.com>
2017-10-14 11:51:04 -0400

    First query.

--- src/apiqueries.js
+++ src/apiqueries.js
@@ -1,5 +1,18 @@
-function getPersons() {
+var API_URL = "http://somewhere/"
 
+function apiUrl(pathElements) {
+  return API_URL + "/" + pathElements.join("/");
+}
+
+function getPersons() {
+  $.getJSON(API_URL, ["people"])
+    .done(function( json ) {
+      console.log( "JSON Data: " + json);
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+  });
 }
 
 function addPerson(name, phoneNumber, schedule, messages) {
